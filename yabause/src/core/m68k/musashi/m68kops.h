fec6e18cc6f00933f6303a983935a44302f3075f
barbudreadmon <barbudreadmon@gmail.com>
2023-02-06 19:25:47 +0100

    (libretro) update files

--- yabause/src/core/m68k/musashi/m68kops.h
+++ yabause/src/core/m68k/musashi/m68kops.h
@@ -1,6 +1,9 @@
 #ifndef M68KOPS__HEADER
 #define M68KOPS__HEADER
 
+#ifdef __cplusplus
+extern "C" {
+	#endif
 /* ======================================================================== */
 /* ============================ OPCODE HANDLERS =========================== */
 /* ======================================================================== */
@@ -1971,6 +1974,7 @@ void m68k_op_unpk_16_mm(void);
 /* Build the opcode handler table */
 void m68ki_build_opcode_table(void);
 
+
 extern void (*m68ki_instruction_jump_table[0x10000])(void); /* opcode handler jump table */
 extern unsigned char m68ki_cycles[][0x10000];
 
@@ -1978,6 +1982,11 @@ extern unsigned char m68ki_cycles[][0x10000];
 /* ======================================================================== */
 /* ============================== END OF FILE ============================= */
 /* ======================================================================== */
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* M68KOPS__HEADER */
 
